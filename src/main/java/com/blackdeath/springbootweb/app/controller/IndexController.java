ceb64e9c9699ad2deb5ec631e35816f2ef3d5682
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2019-05-19 16:21:57 -0500

    Agregar vista index.html
    Se agrega index.html configurado con etiquetas de Thymeleaf y se le pasan
    datos desde IndexController utilizando la interfaz Model y su método
    addAtribute().

--- src/main/java/com/blackdeath/springbootweb/app/controller/IndexController.java
+++ src/main/java/com/blackdeath/springbootweb/app/controller/IndexController.java
@@ -4,6 +4,7 @@
 package com.blackdeath.springbootweb.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -14,7 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
 	@GetMapping({ "", "/", "/index", "/home" })
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("titulo", "Hola Spring Framework!");
 		return "index";
 	}
 
