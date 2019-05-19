8c7628a189c048f2cc475b09165671c47716f6a5
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2019-05-19 16:02:10 -0500

    Cambio de anotación de mapeo
    Se cambia la anotación @RequestMapping por @GetMapping que es un shortcut
    para el @RequestMapping (method=RequestMethod.GET). También existen
    @PostMapping y similares

--- src/main/java/com/blackdeath/springbootweb/app/controller/IndexController.java
+++ src/main/java/com/blackdeath/springbootweb/app/controller/IndexController.java
@@ -4,8 +4,7 @@
 package com.blackdeath.springbootweb.app.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @author Seth Luis
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
 
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	@GetMapping(value = "/index")
 	public String index() {
 		return "index";
 	}
