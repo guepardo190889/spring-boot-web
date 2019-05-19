522e88382ea0ff2e2e8eae4053d7b03504d335f6
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2019-05-19 16:13:14 -0500

    Mapeo múltiple de paths
    Si se agregan llaves, es posible crear un mapa con los diferentes paths
    que mapeará un mismo método

--- src/main/java/com/blackdeath/springbootweb/app/controller/IndexController.java
+++ src/main/java/com/blackdeath/springbootweb/app/controller/IndexController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-	@GetMapping("/index")
+	@GetMapping({ "", "/", "/index", "/home" })
 	public String index() {
 		return "index";
 	}
