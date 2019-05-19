9fc1d4cc6c80493e99e78462ab888933dfec426c
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2019-05-19 16:30:01 -0500

    Utilizar interfaz Model por simplicidad
    Por simplicidad, see utiliza normalmente la interfaz Model para pasar
    datos a la vista y se retorna el nombre de la vista en la que se
    cargarán los datos

--- src/main/java/com/blackdeath/springbootweb/app/controller/IndexController.java
+++ src/main/java/com/blackdeath/springbootweb/app/controller/IndexController.java
@@ -4,8 +4,8 @@
 package com.blackdeath.springbootweb.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Seth Luis
@@ -15,11 +15,10 @@ import org.springframework.web.servlet.ModelAndView;
 public class IndexController {
 
 	@GetMapping({ "", "/", "/index", "/home" })
-	public ModelAndView index(ModelAndView model) {
-		model.addObject("titulo", "Hola Spring Framework!");
-		model.setViewName("index");
+	public String index(Model model) {
+		model.addAttribute("titulo", "Hola Spring Framework!");
 
-		return model;
+		return "index";
 	}
 
 }
