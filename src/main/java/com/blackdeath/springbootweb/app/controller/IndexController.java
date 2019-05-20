bd0be16b5259e9c72111313e21f82d2206d74232
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2019-05-19 21:30:15 -0500

    Agregar email y página de lista de usuarios
    Se agrega al perfil el email del usuario el cuál se mostrará solo si el
    usuario tiene un email, de lo contrario se mostrará un mensaje indicando
    que el usuario no tiene email.
    
    Se crea también página de lista de usuarios, en la cual si no existieran
    usuarios muestra un mensaje informativo

--- src/main/java/com/blackdeath/springbootweb/app/controller/IndexController.java
+++ src/main/java/com/blackdeath/springbootweb/app/controller/IndexController.java
@@ -3,6 +3,9 @@
  */
 package com.blackdeath.springbootweb.app.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,11 +33,22 @@ public class IndexController {
 		Usuario usuario = new Usuario();
 		usuario.setNombre("Seth");
 		usuario.setApellido("Luis");
+		usuario.setEmail("ing.seth.luis@gmail.com");
 
 		model.addAttribute("usuario", usuario);
 		model.addAttribute("titulo", "Perfil del usuario ".concat(usuario.getNombre()));
 
 		return "perfil";
+	}
+
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+		List<String> usuarios = new ArrayList<>();
+
+		model.addAttribute("titulo", "Listado de usuarios");
+		model.addAttribute("usuarios", usuarios);
+
+		return "listar";
 	}
 
 }
--- src/main/java/com/blackdeath/springbootweb/app/model/Usuario.java
+++ src/main/java/com/blackdeath/springbootweb/app/model/Usuario.java
@@ -10,6 +10,7 @@ package com.blackdeath.springbootweb.app.model;
 public class Usuario {
 	private String nombre;
 	private String apellido;
+	private String email;
 
 	public String getNombre() {
 		return nombre;
@@ -25,6 +26,14 @@ public class Usuario {
 
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 }
