0135fc522086f5c04f39ddafb4aea480c44ff3e3
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2019-05-19 22:15:44 -0500

    Ejemplo de parámetros GET con RequestParam
    Se realiza ejemplo para aprender el funcionamiento de la anotación
    @RequestParam, la cual sirve para recuperar parámetros enviados por la URL
    de la aplicación

---
+++ src/main/java/com/blackdeath/springbootweb/app/controller/EjemploParamsController.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.blackdeath.springbootweb.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author Seth Luis
+ *
+ */
+@Controller
+@RequestMapping("/params")
+public class EjemploParamsController {
+
+	@GetMapping("/string")
+	public String param(@RequestParam(required = false, defaultValue = "no se encontró el parámetro") String texto,
+			Model model) {
+		model.addAttribute("resultado", "El parámetro enviado es: " + texto);
+		model.addAttribute("titulo", "Recibir parámetros del request HTTP GET por URL");
+		return "params/ver";
+	}
+}
