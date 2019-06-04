3db0d404805e68c1dfeacb48bf706e6d1bb4e8e4
Seth Karim Luis Martinez <guepardo190889@gmail.com>
2019-06-03 22:24:38 -0500

    Recibir múltiples parámetros con @PatVariable
    Se crea variables/index.html
    
    Se crea ejemplo para recibir múltiples parametros desde la url utilizando
    @PathVariable

--- src/main/java/com/blackdeath/springbootweb/app/controller/EjemploVariablesRutaController.java
+++ src/main/java/com/blackdeath/springbootweb/app/controller/EjemploVariablesRutaController.java
@@ -10,6 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/variables")
 public class EjemploVariablesRutaController {
 
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("titulo", "Enviar parámetros de la ruta (@PathVariable)");
+		return "variables/index";
+	}
+
 	/**
 	 * Este método se puede probar ejecutando en la url:
 	 * http://localhost:8080/variables/string/enviar_texto
@@ -22,6 +28,23 @@ public class EjemploVariablesRutaController {
 	public String variables(@PathVariable String texto, Model model) {
 		model.addAttribute("titulo", "Recibir parámetros de la ruta (@PathVariable");
 		model.addAttribute("resultado", "El texto enviado en la ruta es: " + texto);
+		return "variables/ver";
+	}
+
+	/**
+	 * Este método se puede probar ejecutando en la url:
+	 * http://localhost:8080/variables/string/Saludos%20Seth/7
+	 * 
+	 * @param texto
+	 * @param numero
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/string/{texto}/{numero}")
+	public String variables(@PathVariable String texto, @PathVariable Integer numero, Model model) {
+		model.addAttribute("titulo", "Recibir parámetros de la ruta (@PathVariable");
+		model.addAttribute("resultado",
+				"El texto enviado en la ruta es: " + texto + " y el nùmero enviado en el path es: " + numero);
 		return "variables/ver";
 	}
 
