d0df1d05fb3261be228e75fca70144bcf8a4dd86
Seth Karim Luis Martinez <guepardo190889@gmail.com>
2019-06-30 00:12:13 -0500

    Ejemplo forward y redirect
    Se crea ejemplo para forward y redirect

---
+++ src/main/java/com/blackdeath/springbootweb/app/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.blackdeath.springbootweb.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		//return "forward:/app/index";
+		return "forward:/app/index";
+	}
+}
