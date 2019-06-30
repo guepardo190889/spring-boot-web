6c6a44a48a558667da14fdd81fa3ea1825fb9464
Seth Karim Luis Martinez <guepardo190889@gmail.com>
2019-06-29 19:09:23 -0500

    Creación de properties personalizado
    Se crea textos.properties y clase TextosPropertiesConfig para configurar
    el nuevo properties.
    
    Nota: La carpeta resources es el classpath

---
+++ src/main/java/com/blackdeath/springbootweb/app/TextosPropertiesConfig.java
@@ -0,0 +1,11 @@
+package com.blackdeath.springbootweb.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({ @PropertySource("classpath:textos.properties") })
+public class TextosPropertiesConfig {
+
+}
